fa723041c74bf8330d4658b2f67ab4d67e7a72d6
Leydy Tarazona <leydy.tarazona@gmail.com>
2024-12-16 22:20:50 -0500

    added environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  api: {
+    base: 'https://hi-api-develop.helloinsight.org',
+    services: {
+      groupsList: '/tech-test-groups-list/',
+      trackEvent: '/tech-test-track-event/',
+      readEvents: '/tech-test-read-events/',
+    }
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  api: {
+    base: 'https://hi-api-develop.helloinsight.org',
+    services: {
+      groupsList: '/tech-test-groups-list/',
+      trackEvent: '/tech-test-track-event/',
+      readEvents: '/tech-test-read-events/',
+    }
+  },
+};
